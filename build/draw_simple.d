c3434c4ed1eb8a5a0ee3715dd8839ecd2ee06552
Antoine Josse <ajosse@student.42.fr>
2025-02-01 01:19:17 +0100

    ray casting V1.2

--- build/draw_simple.d
+++
@@ -1,6 +0,0 @@
-build/draw_simple.o: srcs/ray_casting/draw_simple.c \
- srcs/ray_casting/../../includes/cub3d.h \
- srcs/ray_casting/../../includes/../libft/libft.h \
- srcs/ray_casting/../../includes/../libft/ft_printf.h \
- srcs/ray_casting/../../includes/../libft/get_next_line/get_next_line.h \
- srcs/ray_casting/../../includes/../minilibx-linux/mlx.h
--- build/raycast.d
+++
@@ -1,6 +0,0 @@
-build/raycast.o: srcs/ray_casting/raycast.c \
- srcs/ray_casting/../../includes/cub3d.h \
- srcs/ray_casting/../../includes/../libft/libft.h \
- srcs/ray_casting/../../includes/../libft/ft_printf.h \
- srcs/ray_casting/../../includes/../libft/get_next_line/get_next_line.h \
- srcs/ray_casting/../../includes/../minilibx-linux/mlx.h
